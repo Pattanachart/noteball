8e5c4e6363a47a7dc2bf1cc5f2dfaa854845ead4
GREEN gph <green311_@outlook.com>
2023-08-11 17:24:49 +0700

    edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
+
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'; // นำเข้าโมดูล path มาใช้งาน
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +28,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, 'src')
     }
   }
 })
