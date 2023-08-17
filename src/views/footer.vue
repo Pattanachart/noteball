16bd47888b80a524f7b39c966792b4fe7ff499c4
GREEN gph <green311_@outlook.com>
2023-08-18 02:11:46 +0700

    add footer

--- src/views/footer.vue
+++ src/views/footer.vue
@@ -4,6 +4,15 @@
             <h1> <span> <strong>©</strong> </span> Dev by Pattanachart </h1>
             <p>© 2023 All Rights Reserved. Design by Pattanachart</p>
             <!-- <p @click="showConfetti">AAAAA</p> -->
+
+            <div class="container">
+                <div class="d-flex">
+                    <div class="col-md-3 col-3">
+                        <hr>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     </div>
 </template>
@@ -22,6 +31,14 @@
 * {
     /* border: 1px solid black; */
 }
+hr{
+    
+    border: 1.5px double rgb(211, 204, 204,0.4);
+    width: 100%;
+}
+.container{
+    margin-bottom: -5rem ;
+}
 .container-fluid{
     left: 0;
     bottom: 0;
@@ -31,16 +48,7 @@
     margin-top: 1.5em;
   
 }
-#footer {
-    /* position: absolute; */
-    /* left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: rgb(247, 241, 241);
-    text-align: center;
-    margin-top: 1.5em; */
 
-}
 
 h1,
 p {
@@ -57,11 +65,4 @@ p {
     }
 
 }
-/* @media screen and (max-width: 1000px) {
-
-    h1,
-    p {
-        font-size: 10px;
-    } 
-}*/
 </style>
